38d1d03327035aadaade1a95ee60770f6236b49a
Jhon <jhonlz2011@hotmail.com>
2024-04-11 21:24:23 -0500

    Github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://Jhonlz09.github.io.git";
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Jhonlz09.github.io.git'
+});
